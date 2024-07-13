fcbfacbe54335dce61e2e6d26bb025ca65cadaa6
Kai Roy <kai7lego+github@gmail.com>
2024-07-13 12:57:36 -0700

    Update RISC-V_R_instr_tb.sv

--- RV32I - Single Cycle/tb/RISC-V_R_instr_tb.sv
+++ RV32I - Single Cycle/tb/RISC-V_R_instr_tb.sv
@@ -62,7 +62,7 @@ module tb();
 	initial begin
 		#10 //ADD
 		display_b_txt("Test 1: ADD");
-		$cast({bus.idata[30], bus.idata[25], bus.idata[14:12]}, ADD);
+		$cast({idata[30], idata[25], idata[14:12]}, ADD);
 		rv1=32'd415;
 		rv2=32'd60;
 		#1 display_state();
@@ -73,7 +73,7 @@ module tb();
 
 		#10 //SUB
 		display_b_txt("Test 2: SUB");
-		$cast({bus.idata[30], bus.idata[25], bus.idata[14:12]}, SUB);
+		$cast({idata[30], idata[25], idata[14:12]}, SUB);
 		rv1=32'd6553;
 		rv2=32'd653;
 		#1 display_state();
@@ -84,7 +84,7 @@ module tb();
 
 		#10 //SLL
 		display_b_txt("Test 3: SLL");
-		$cast({bus.idata[30], bus.idata[25], bus.idata[14:12]}, SLL);
+		$cast({idata[30], idata[25], idata[14:12]}, SLL);
 		rv1=32'd288;
 		rv2=32'd349;
 		#1 display_state();
@@ -95,7 +95,7 @@ module tb();
 
 		#10 //SLT
 		display_b_txt("Test 4: SLT");
-		$cast({bus.idata[30], bus.idata[25], bus.idata[14:12]}, SLT);
+		$cast({idata[30], idata[25], idata[14:12]}, SLT);
 		rv1=32'd696;
 		rv2=32'd623;
 		#1 display_state();
@@ -106,7 +106,7 @@ module tb();
 
 		#10 //SLTU
 		display_b_txt("Test 5: SLTU");
-		$cast({bus.idata[30], bus.idata[25], bus.idata[14:12]}, SLTU);
+		$cast({idata[30], idata[25], idata[14:12]}, SLTU);
 		rv1=32'd447;
 		rv2=32'd726;
 		#1; display_state();
@@ -117,7 +117,7 @@ module tb();
 
 		#10 //XOR
 		display_b_txt("Test 6: XOR");
-		$cast({bus.idata[30], bus.idata[25], bus.idata[14:12]}, XOR);
+		$cast({idata[30], idata[25], idata[14:12]}, XOR);
 		rv1=32'd696;
 		rv2=32'd939;
 		#1 display_state();
@@ -128,7 +128,7 @@ module tb();
 
 		#10 //SRL
 		display_b_txt("Test 7: SRL");
-		$cast({bus.idata[30], bus.idata[25], bus.idata[14:12]}, SRL);
+		$cast({idata[30], idata[25], idata[14:12]}, SRL);
 		rv1=32'd147;
 		rv2=32'd194;
 		#1 display_state();
@@ -139,7 +139,7 @@ module tb();
 
 		#10 //SRA
 		display_b_txt("Test 8: SRA");
-		$cast({bus.idata[30], bus.idata[25], bus.idata[14:12]}, SRA);
+		$cast({idata[30], idata[25], idata[14:12]}, SRA);
 		rv1=32'd848;
 		rv2=32'd325;
 		#1 display_state();
@@ -150,7 +150,7 @@ module tb();
 
 		#10 //OR
 		display_b_txt("Test 9: OR");
-		$cast({bus.idata[30], bus.idata[25], bus.idata[14:12]}, OR);
+		$cast({idata[30], idata[25], idata[14:12]}, OR);
 		rv1=32'd378;
 		rv2=32'd960;
 		#1 display_state();
@@ -161,7 +161,7 @@ module tb();
 
 		#10 //AND
 		display_b_txt("Test 10: AND");
-		$cast({bus.idata[30], bus.idata[25], bus.idata[14:12]}, AND);
+		$cast({idata[30], idata[25], idata[14:12]}, AND);
 		rv1=32'd404;
 		rv2=32'd900;
 		#1 display_state();
