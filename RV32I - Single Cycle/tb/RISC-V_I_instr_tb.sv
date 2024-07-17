b06d1c16074a3ece789de45cf634764c70e48df2
Kai Roy <kai7lego+github@gmail.com>
2024-07-17 07:51:57 -0700

    Update RISC-V_I_instr_tb.sv

--- RV32I - Single Cycle/tb/RISC-V_I_instr_tb.sv
+++ RV32I - Single Cycle/tb/RISC-V_I_instr_tb.sv
@@ -61,8 +61,8 @@ module tb;
 		#10 //ADDI
 		display_b_txt("Test 1: ADDI");
 		$cast({idata[14:12]}, ADDI);
-		rv1=2147483648;
-		imm=3;
+		rv1=1073741824;
+		imm=1073741824;
 		#1 display_state();
 		if (rd == (rv1 + imm))
 			display_pass("PASS");
