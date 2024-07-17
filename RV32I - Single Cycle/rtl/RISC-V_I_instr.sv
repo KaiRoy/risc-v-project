544070cd2b16e4b58ff883c7a17d845731ece729
Kai Roy <kai7lego+github@gmail.com>
2024-07-17 07:29:25 -0700

    Update RISC-V_I_instr.sv

--- RV32I - Single Cycle/rtl/RISC-V_I_instr.sv
+++ RV32I - Single Cycle/rtl/RISC-V_I_instr.sv
@@ -38,7 +38,7 @@ module I_type(Instr_IO.I_type_io_ports bus);
 			ORI:	rd = rs1 | imm;
 			ANDI:	rd = rs1 & imm;
 			SLLI:  	rd = rs1<<imm[4:0];
-            SRLI: 	rd = bus.idata[30] ? rs1>>imm[4:0] : rs1>>>imm[4:0]; //srli || srai
+            SRLI: 	rd = bus.idata[30] ? (rs1>>imm[4:0]) : (rs1>>>imm[4:0]); //srli || srai
 		    default: ;
 		endcase
 
