adc57df2873018ed35fe49a6e7545481d8a0aa99
Kai Roy <kai7lego+github@gmail.com>
2025-06-18 15:13:35 -0700

    Setup IDE for Single Cycle C Model

---
+++ C-Models/RISC-V-Single-Cycle/build/CMakeFiles/RISC-V-Single-Cycle.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RISC-V-Single-Cycle.dir/src/main.cpp.obj"
+  "CMakeFiles/RISC-V-Single-Cycle.dir/src/main.cpp.obj.d"
+  "RISC-V-Single-Cycle.exe"
+  "RISC-V-Single-Cycle.exe.manifest"
+  "RISC-V-Single-Cycle.pdb"
+  "libRISC-V-Single-Cycle.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RISC-V-Single-Cycle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C-Models/RISC-V-Single-Cycle/build/CMakeFiles/RISC-V-Single-Cycle.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RISC-V-Single-Cycle.
+# This may be replaced when dependencies are built.
